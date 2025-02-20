269d381b38506089e09438418070f01d023153b6
Maciej Tonderski <maciejtonderski.mt@gmail.com>
2025-02-20 20:16:07 +0000

    add validation for login

--- pages/Login/jsobjects/Login_and_store_token/Login_and_store_token.js
+++ pages/Login/jsobjects/Login_and_store_token/Login_and_store_token.js
@@ -1,16 +1,21 @@
 export default {
 	Button1onClick() {
-		LoginQuery.run()
-			.then((response) => {  // Run after the query is successful
-			if (response && response.access_token) {
-				storeValue("JWT_TOKEN", response.access_token, persist=True); // Save the token
-				showAlert("Login Successful", "success");
-				navigateTo("Home Page")
-			} else {
-				showAlert("Login failed: No token received", "error");
-			}
-		})
-			.catch(() => showAlert("Unsuccessful login", "error")); // Run if the query encounters any errors
-		return true;
-	}
+		if(LoginInput.isValid && PasswordInput.isValid){
+			LoginQuery.run()
+				.then((response) => {  // Run after the query is successful
+				if (response && response.access_token) {
+					storeValue("JWT_TOKEN", response.access_token, persist=True); // Save the token
+					showAlert("Login Successful", "success");
+					navigateTo("Home Page")
+				} else {
+					showAlert("Login failed: No token received", "error");
+				}
+			})
+				.catch(() => showAlert("Unsuccessful login", "error")); // Run if the query encounters any errors
+			return true;
+		}else {
+			showAlert("Please provide valid login and password values")
+		}
+	} 
+
 }
