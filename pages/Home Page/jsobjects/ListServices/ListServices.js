ece65adb54455d5e94c560c80fb32d30db5c1061
Maciej Tonderski <maciejtonderski.mt@gmail.com>
2025-03-02 20:19:27 +0000

    demo mode on list

--- pages/Home Page/jsobjects/ListServices/ListServices.js
+++ pages/Home Page/jsobjects/ListServices/ListServices.js
@@ -8,12 +8,29 @@ export default {
 			if (response) {
 				storeValue("services", response);
 			} else {
-				storeValue("services", []);
+				storeValue("services", [{"name": "demo", "status": "stopped"}]);
 				showAlert("Error: Invalid response received", "error");
 			}
 		} catch (e) {
 
 		}
 		return true;
+	},
+
+	intervalId: null,  // Store the interval ID for clearing
+
+	async startAutoRefresh() {
+		if (AutoRefreshInstance.isSwitchedOn) {
+			if (!this.intervalId) {  // Prevent multiple intervals
+				this.intervalId = setInterval(async () => {
+					await this.listServicesQuery()();
+				}, 5000);
+			}
+		} else {
+			if (this.intervalId) {
+				clearInterval(this.intervalId);
+				this.intervalId = null;  // Reset interval ID
+			}
+		}
 	}
 }
