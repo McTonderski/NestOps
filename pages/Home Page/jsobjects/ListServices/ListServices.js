6147d4ac17ad99f2e7bef53e8031bb0af98412b4
Maciej Tonderski <maciejtonderski.mt@gmail.com>
2025-03-02 20:07:45 +0000

    modify scripts

--- pages/Home Page/jsobjects/ListServices/ListServices.js
+++ pages/Home Page/jsobjects/ListServices/ListServices.js
@@ -1,56 +1,19 @@
 export default {
-	const response = await getInstanceDetails.run();  // Run API request
+	async listServicesQuery() {
+		try {
+			const response = await ServiceListGet.run();  // Run API request
 			if(appsmith.store.JWTToken === "" || response.status === 401){
 				navigateTo("Login Page")
 			}
 			if (response) {
-				// Extract necessary details
-				const cpuUsage = response.cpu_usage_percent;  // CPU usage in percentage
-				const memoryUsage = response.memory_usage.percent;  // Memory usage in percentage
-				response.running_docker_containers.forEach(container => {
-					if (container.status === "running") {
-						containerSummary.find(item => item.x === "Running").y++;
-					} else if (container.status === "exited") {
-						containerSummary.find(item => item.x === "Stopped").y++;
-					} else {
-						containerSummary.find(item => item.x === "Errors").y++;
-					}
-				});
-				const servicesCount = containerSummary.reduce((sum, item) => sum + item.y, 0);
-				// Store values in Appsmith
-				storeValue("CPU_USAGE", cpuUsage);
-				storeValue("MEMORY_USAGE", memoryUsage);
-				storeValue("SERVICE_COUNT", servicesCount);
-				storeValue("SERVICE_LIST", response.running_docker_containers);
-				storeValue("SERVICE_STATS", containerSummary)
-
+				storeValue("services", response);
 			} else {
+				storeValue("services", []);
 				showAlert("Error: Invalid response received", "error");
 			}
 		} catch (e) {
-			storeValue("CPU_USAGE", "N/A");
-			storeValue("MEMORY_USAGE", "N/A");
-			storeValue("SERVICE_COUNT", "N/A");
-			storeValue("SERVICE_LIST", containerSummary);
-			storeValue("SERVICE_STATS", "N/A")
+
 		}
 		return true;
-	},
-
-	intervalId: null,  // Store the interval ID for clearing
-
-	async startAutoRefresh() {
-		if (AutoRefreshInstance.isSwitchedOn) {
-			if (!this.intervalId) {  // Prevent multiple intervals
-				this.intervalId = setInterval(async () => {
-					await this.queryAutoRefresh();
-				}, 5000);
-			}
-		} else {
-			if (this.intervalId) {
-				clearInterval(this.intervalId);
-				this.intervalId = null;  // Reset interval ID
-			}
-		}
 	}
 }
